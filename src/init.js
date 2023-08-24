4651716bf2ee95b0f3fdcf37d55ceef53b31c662
yunhsiaho <yunhsia.ho@gmail.com>
2023-08-24 16:59:05 +0200

    finish basic adding task function and clear all

--- src/init.js
+++ src/init.js
@@ -1,2 +1,33 @@
-const data = {};
-console.log('Let us build a tod list');
+console.log('start');
+
+// click button to get input value
+document.querySelector('button.add-task').addEventListener("click",()=>{
+    let input =  document.querySelector('.searchbar');
+    console.log(input.value);
+
+    if(input.value != ""){
+            // if searchbar is not empty, create a new task line in task container
+        let taskContainer = document.querySelector('.task-container');
+        let task = document.createElement('div');
+        let hr = document.createElement('hr');
+        task.className = 'task-line';
+
+        task.innerHTML = input.value;
+        task.append(hr);
+        taskContainer.append(task);
+    }
+
+    // clear the searchbar after adding task
+    input.value = "";
+    console.log(input.value);
+})
+
+//clear all
+document.querySelector('button.clear-all').addEventListener("click", ()=>{
+    console.log('remove!');
+    
+    let taskContainer = document.querySelector('.task-container');
+    while (taskContainer.firstChild) {
+        taskContainer.removeChild(taskContainer.firstChild);
+    }
+})
